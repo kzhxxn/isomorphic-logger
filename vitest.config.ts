61aed814c3ff3bc35ca4288fd06917b29850f6ed
Jihun Kang <kzhxxn@gmail.com>
2025-11-28 17:37:44 +0900

    chore: fix gitignore dist/

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "node",
+    exclude: ["dist/**", "node_modules/**"],
     coverage: {
       reporter: ["text", "json", "html"],
     },
